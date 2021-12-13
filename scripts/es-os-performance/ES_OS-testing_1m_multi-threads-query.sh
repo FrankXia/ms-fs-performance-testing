0dde3a9a5441a6b6f2c109dfc40f6a35754d981b
Frank Xia <FXia@esri.com>
2021-12-13 15:11:18 -0500

    moved scripts for ES-OS testing cases to a new folder under scripts

---
+++ scripts/es-os-performance/ES_OS-testing_1m_multi-threads-query.sh
@@ -0,0 +1,35 @@
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl   5 100 ./test_outputs/query/planes_1m_fl_querying_1k_5thread_100req.txt ./random_extents/1m_extents_1k_features.txt 0
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  10 200 ./test_outputs/query/planes_1m_fl_querying_1k_10thread_200req.txt ./random_extents/1m_extents_1k_features.txt 100
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  20 400 ./test_outputs/query/planes_1m_fl_querying_1k_20thread_400req.txt ./random_extents/1m_extents_1k_features.txt 300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  30 600 ./test_outputs/query/planes_1m_fl_querying_1k_30thread_600req.txt ./random_extents/1m_extents_1k_features.txt 700
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  40 800 ./test_outputs/query/planes_1m_fl_querying_1k_40thread_800req.txt ./random_extents/1m_extents_1k_features.txt 1300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl 50 1000 ./test_outputs/query/planes_1m_fl_querying_1k_50thread_1000req.txt ./random_extents/1m_extents_1k_features.txt 2100
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl   5 100 ./test_outputs/query/planes_1m_fl_querying_5k_5thread_100req.txt ./random_extents/1m_extents_5k_features.txt 0
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  10 200 ./test_outputs/query/planes_1m_fl_querying_5k_10thread_200req.txt ./random_extents/1m_extents_5k_features.txt 100
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  20 400 ./test_outputs/query/planes_1m_fl_querying_5k_20thread_400req.txt ./random_extents/1m_extents_5k_features.txt 300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  30 600 ./test_outputs/query/planes_1m_fl_querying_5k_30thread_600req.txt ./random_extents/1m_extents_5k_features.txt 700
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  40 800 ./test_outputs/query/planes_1m_fl_querying_5k_40thread_800req.txt ./random_extents/1m_extents_5k_features.txt 1300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl 50 1000 ./test_outputs/query/planes_1m_fl_querying_5k_50thread_1000req.txt ./random_extents/1m_extents_5k_features.txt 2100
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl   5 100 ./test_outputs/query/planes_1m_fl_querying_10k_5thread_100req.txt ./random_extents/1m_extents_10k_features.txt 0
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  10 200 ./test_outputs/query/planes_1m_fl_querying_10k_10thread_200req.txt ./random_extents/1m_extents_10k_features.txt 100
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  20 400 ./test_outputs/query/planes_1m_fl_querying_10k_20thread_400req.txt ./random_extents/1m_extents_10k_features.txt 300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  30 600 ./test_outputs/query/planes_1m_fl_querying_10k_30thread_600req.txt ./random_extents/1m_extents_10k_features.txt 700
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl  40 800 ./test_outputs/query/planes_1m_fl_querying_10k_40thread_800req.txt ./random_extents/1m_extents_10k_features.txt 1300
+sleep 2
+java -cp  ./target/ms-fs-performance-test-1.0-jar-with-dependencies.jar com.esri.arcgis.performance.test.mat.FeatureServiceWithRandomBBoxConcurrentTester https://us-iotdev.arcgis.com/opensearch03/cqvgkj9zrnkn9bcu/maps/arcgis/rest/services planes_1m_bat_fl 50 1000 ./test_outputs/query/planes_1m_fl_querying_10k_50thread_1000req.txt ./random_extents/1m_extents_10k_features.txt 2100
